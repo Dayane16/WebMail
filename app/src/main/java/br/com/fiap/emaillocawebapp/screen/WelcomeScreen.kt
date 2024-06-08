89623ecd2ab40860d8118d663e0ec1e849498b85
Francisca Dayane <daydantas1028@gmail.com>
2024-06-08 15:28:25 -0300

    Defined styles

--- app/src/main/java/br/com/fiap/emaillocawebapp/screen/WelcomeScreen.kt
+++ app/src/main/java/br/com/fiap/emaillocawebapp/screen/WelcomeScreen.kt
@@ -1,19 +1,30 @@
 package br.com.fiap.emaillocawebapp.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import br.com.fiap.emaillocawebapp.ui.theme.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WelcomeScreen(navigateToNext: () -> Unit) {
+        val gradientColors = listOf(
+                RedGrad,
+                White
+        )
+
+        val gradientBrush = createGradientBrush(colors = gradientColors)
+
         Column(
                 modifier = Modifier
                         .fillMaxSize()
+                        .background(gradientBrush) // Aplicar o gradiente aqui
                         .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
--- app/src/main/java/br/com/fiap/emaillocawebapp/ui/theme/Color.kt
+++ app/src/main/java/br/com/fiap/emaillocawebapp/ui/theme/Color.kt
@@ -5,7 +5,15 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Red80 = Color(0xFFF00843)
+val Red40 = Color(0xFFFFB6B6)
+val Red20 = Color(0xFFFDD6E0)
+val DarkBlue = Color(0xFF2A343E)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val RedGrad = Color(0xFFECCFCF)
